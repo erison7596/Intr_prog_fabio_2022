7cbc9139f48afbd5835af57b185e5b93cccbb98b
Erison Cavalcante <erisoncavalcante@eng.ci.ufpb.br>
2022-03-24 09:26:49 -0300

    fim

--- Lista2/questao2.py
+++ Lista2/questao2.py
@@ -2,5 +2,5 @@ base = int(input("Digite a base: "))
 expoente = int(input("Digite o expoente: "))
 resultado = base
 for i in range(1, expoente):
-    base *= base
+    resultado *= base
 print("O resultado é: ", resultado)
